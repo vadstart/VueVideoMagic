9dd2f344b434593b91a7e6b2f55c38a89f44925c
dupasj <dupasj97@gmail.com>
2025-07-08 22:40:01 +0200

    Remove useless stuffs

--- src/components/MyVideo.vue
+++ src/components/MyVideo.vue
@@ -1,36 +1,11 @@
 
 <script setup lang="ts">
 import {useElementVisibility} from "@vueuse/core";
-import {onMounted, onUnmounted, ref, useTemplateRef, watch} from "vue";
+import {useTemplateRef, watch} from "vue";
 
 const props = defineProps<{src: string}>()
 const video = useTemplateRef<HTMLVideoElement>('video')
 const targetIsVisible = useElementVisibility(video)
-
-const userInteracted = ref(false)
-
-function handleUserInteraction() {
-  userInteracted.value = true
-  window.removeEventListener('click', handleUserInteraction)
-  window.removeEventListener('keydown', handleUserInteraction)
-  window.removeEventListener('pointerdown', handleUserInteraction)
-
-  if (video.value) {
-    // video.value.setAttribute('muted',true);
-  }
-}
-
-onMounted(() => {
-  window.addEventListener('click', handleUserInteraction)
-  window.addEventListener('keydown', handleUserInteraction)
-  window.addEventListener('pointerdown', handleUserInteraction)
-})
-
-onUnmounted(() => {
-  window.removeEventListener('click', handleUserInteraction)
-  window.removeEventListener('keydown', handleUserInteraction)
-  window.removeEventListener('pointerdown', handleUserInteraction)
-})
 
 watch([video,targetIsVisible],([video,value]) => {
   if (video === null){
